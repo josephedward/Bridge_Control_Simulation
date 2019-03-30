c37cbbc8d5323a7c82aebe11d3e912920a0c878f
GrimbleLewis <grimblelewisthethird@gmail.com>
2019-03-30 11:18:59 -0400

    added the timeDisplay ajax call function that will display the current time of the bridge selected, added data-type to each bridge in the html of their timezone

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,3 +1,49 @@
+
+
+
+// displays the current time based on the which bridge is selected
+  $(document).on("click", ".bridge-name", function displayTime() {
+  var timezone = $(this).attr("data-type");
+  var timeQueryURL = "http://worldclockapi.com/api/json/" + timezone + "/now"
+  
+  $.ajax({
+  url: timeQueryURL,
+  method: "GET"
+  }).then(function(response) {
+  
+  var utcOffset = (response.utcOffset)
+
+  var currentBridgeTime = moment().utcOffset(utcOffset).format("hh:mm A");
+
+  console.log(currentBridgeTime)
+  });
+  
+  });
+  
+  
+  //object of bridge coords that the api will pull from
+  // var coords = {
+  // buffaloBluff: {
+  // coordinates: "lat=29.59&lon=-81.68"
+  // },
+  // kanawhaCanal: {
+  // coordinates: "lat=37.53&lon=-77.42"
+  // },
+  // pascagoula: {
+  // coordinates: "lat=30.37&lon=-88.56"
+  // },
+  // flaglerMemorial: {
+  // coordinates: "lat=26.72&lon=-80.05"
+  // },
+  // bridgeOfLions: {
+  // coordinates: "lat=29.89&lon=-81.31"
+  // },
+  // harryNice: {
+  // coordinates: "lat=38.36&lon=-76.99"
+  // }
+  // }
+  
+
 //displays weather based on which bridge is clicked
 $(document).on("click", ".bridge-name", function displayWeather() {
   
