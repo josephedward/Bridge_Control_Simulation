b51d442b5bf0b0a35baba9c8132671c432b73c5c
GrimbleLewis <grimblelewisthethird@gmail.com>
2019-03-30 12:18:45 -0400

    added display divs to the html that pushes the weather info into the html, also added a titleCase function for better looking condition-display

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,79 +1,67 @@
-
-
-
 // displays the current time based on the which bridge is selected
-  $(document).on("click", ".bridge-name", function displayTime() {
+$(document).on("click", ".bridge-name", function displayTime() {
   var timezone = $(this).attr("data-type");
-  var timeQueryURL = "http://worldclockapi.com/api/json/" + timezone + "/now"
-  
+  var timeQueryURL = "http://worldclockapi.com/api/json/" + timezone + "/now";
+
   $.ajax({
-  url: timeQueryURL,
-  method: "GET"
+    url: timeQueryURL,
+    method: "GET"
   }).then(function(response) {
-  
-  var utcOffset = (response.utcOffset)
+    var utcOffset = response.utcOffset;
 
-  var currentBridgeTime = moment().utcOffset(utcOffset).format("hh:mm A");
+    var currentBridgeTime = moment()
+      .utcOffset(utcOffset)
+      .format("hh:mm A");
 
-  console.log(currentBridgeTime)
+    console.log(currentBridgeTime);
   });
-  
-  });
-  
-  
-  //object of bridge coords that the api will pull from
-  // var coords = {
-  // buffaloBluff: {
-  // coordinates: "lat=29.59&lon=-81.68"
-  // },
-  // kanawhaCanal: {
-  // coordinates: "lat=37.53&lon=-77.42"
-  // },
-  // pascagoula: {
-  // coordinates: "lat=30.37&lon=-88.56"
-  // },
-  // flaglerMemorial: {
-  // coordinates: "lat=26.72&lon=-80.05"
-  // },
-  // bridgeOfLions: {
-  // coordinates: "lat=29.89&lon=-81.31"
-  // },
-  // harryNice: {
-  // coordinates: "lat=38.36&lon=-76.99"
-  // }
-  // }
-  
+});
 
 //displays weather based on which bridge is clicked
 $(document).on("click", ".bridge-name", function displayWeather() {
-  
-var coord = $(this).attr("data-coord");
-var weatherQueryURL =
-  "http://api.openweathermap.org/data/2.5/weather?" + coord + "&units=imperial&APPID=5adeb498269cc58939e872c2a5c2c0a2";
+  var coord = $(this).attr("data-coord");
+  var weatherQueryURL =
+    "http://api.openweathermap.org/data/2.5/weather?" +
+    coord +
+    "&units=imperial&APPID=5adeb498269cc58939e872c2a5c2c0a2";
 
+  console.log(coord);
+  $.ajax({
+    url: weatherQueryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
 
-console.log(coord)
-$.ajax({
-  url: weatherQueryURL,
-  method: "GET"
-}).then(function(response) {
+    var temp = response.main.temp;
+    var humidity = response.main.humidity;
+    var conditions = response.weather[0].description;
+    var windSpeed = response.wind.speed;
+    var windDegress = response.wind.deg;
 
- console.log(response)
+    // function to capitalize first letter of each word in a string
+    var titleCase = function(str) {
+      var result = [];
+      var words = str.split(" ");
+      for (var i = 0; i < words.length; i++) {
+        var word = words[i].split("");
+        word[0] = word[0].toUpperCase();
+        result.push(word.join(""));
+      }
+      return result.join(" ");
+    };
 
-var temp = response.main.temp;
-var humidity = response.main.humidity;
-var conditions = response.weather[0].description;
-var windSpeed = response.wind.speed;
-var windDegress = response.wind.deg;
+    var conditionsUppercase = titleCase(conditions);
 
-$("#temperature").text().temp
+    $("#temp-display").text(`Current Temperature: ${temp}°F`);
+    $("#humidity-display").text(`Humidity: ${humidity}%`);
+    $("#conditions-display").text(`Conditions: ${conditionsUppercase}`);
+    $("#windSpeed-display").text(`Wind Speed: ${windSpeed}mph`);
+    $("#windDegrees-display").text(`Wind Degrees: ${windDegress}°`);
 
-console.log(temp);
-console.log(humidity);
-console.log(conditions);
-console.log(windSpeed);
-console.log(windDegress);
-})
-
-
+    console.log(temp);
+    console.log(humidity);
+    console.log(conditions);
+    console.log(windSpeed);
+    console.log(windDegress);
+  });
 });
