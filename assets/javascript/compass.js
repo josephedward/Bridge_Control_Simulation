d67df5fb5d138fbefebd19e1044027f5ee0f3646
O <josephvesleywork@gmail.com>
2019-04-03 21:13:42 -0400

    tried to fix location of images

--- assets/javascript/compass.js
+++ assets/javascript/compass.js
@@ -54,11 +54,11 @@ function init() {
 
 		// Load the needle image
 		needle = new Image();
-		needle.src = '../images/smallNeedle.png';
+		needle.src = '/../assets/images/smallNeedle.png';
 
 		// Load the compass image
 		img = new Image();
-		img.src = '../images/smallCompass.png';
+		img.src = '/../assets/images/smallCompass.png';
 		img.onload = imgLoaded;
 	} else {
 		alert("Canvas not supported!");
