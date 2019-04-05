ffc646d36ed9fbf234645a3aed173b6f32c23f3d
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-04-04 21:07:23 -0400

    bug fixes, forward and reverse functionality added, cannot operate without bridge selection, log in added back, form validation on log in tested, alerts removed from log in, bridge animations and timing updated.

--- assets/javascript/bridgeAnimation.js
+++ assets/javascript/bridgeAnimation.js
@@ -1,34 +1,56 @@
+//  create a boolean to prevent double movement of the bridge
 var bridgeOpen = false;
+
+//create a variable to hold iterator
 var i = 1;
+//create a function to animate the bridge
+function animateBridgeOpen() {
+  //check bridge state
 
-function animateBridgeOpen () {
-    var bridgeMove = setInterval( function() {
-    i++;
-    
-    $("#bridge-image").attr("src", "assets/images/Bridge"+i+".JPG");
-    if (i === 5) {
+  if (bridgeOpen === false) {
+
+    //loop throught bridge images on an interval
+    var bridgeMove = setInterval(function() {
+      i++;
+      //update the bridge image with the new iteration
+      $("#bridge-image").attr("src", "assets/images/Bridge" + i + ".JPG");
+      //stop the iteration at 5 (number of bridge images we have)
+      if (i === 5) {
+        //clear the interval when we hit five
         clearInterval(bridgeMove);
-    }
-    console.log(i);
-    }, 250)
-    bridgeOpen = true;
-    return i;
-
-}
-
-
-function animateBridgeClose () {
-    var bridgeMove = setInterval( function() {
-    i--;
+        $switch5.text("BRIDGE OPEN");
+        bridgeOpen = true;
+        $switch5.toggleClass("transition")
+        $("#operate-button").toggleClass("transition");
+        $("#operate-button").text("CLOSE");
+      }
+      // set operation speed
+    }, 250);
+    //change the boolean to prevent double operation
     
-    $("#bridge-image").attr("src", "assets/images/Bridge"+i+".JPG")
-    if (i === 1) {
-        clearInterval(bridgeMove);
-    }
-    console.log(i);
-    }, 250)
-    bridgeOpen = false;
     return i;
+  }
+} 
+
+function animateBridgeClose() {
+  //check bridge state
+  if ((bridgeOpen = true)) {
+    //interval for decrementing through bridge animations
+    var bridgeMove = setInterval(function() {
+      i--;
+      //update bridge image
+      $("#bridge-image").attr("src", "assets/images/Bridge" + i + ".JPG");
+
+      //stop animation when bridge image gets to first image
+      if (i === 1) {
+        clearInterval(bridgeMove);
+        $switch5.text("BRIDGE CLOSED");
+        bridgeOpen = false;
+        $switch5.toggleClass("released")
+        
+      }
+    }, 250);
+
+    return i;
+  }
 }
-
-
